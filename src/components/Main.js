d5ad48d02b0a4022544090ba095efbc7625c7665
Vlad-maker <skateislife@mail.ru>
2021-02-10 18:07:31 +0300

    Feat: catch error added

--- src/components/Main.js
+++ src/components/Main.js
@@ -24,6 +24,9 @@ export default function Main(props) {
             .then((data) => {
                 setCards(data);
             })
+            .catch((err) => {
+                console.log(err)
+            });
     }, []);
 
     return (
